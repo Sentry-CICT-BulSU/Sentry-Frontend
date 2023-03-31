c5c476196119e4cccb979540e9876a46c077f66e
rhonstratos <kizunaai20@gmail.com>
2023-03-31 16:37:53 +0800

    angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,26 @@ import { ResponsiveHelperComponent } from './components/responsive-helper/respon
 import { UserTableComponent } from './components/user-table/user-table.component';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
 
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-  declarations: [ResponsiveHelperComponent, UserTableComponent, ClickOutsideDirective],
-  imports: [CommonModule],
-  exports: [ResponsiveHelperComponent, UserTableComponent, ClickOutsideDirective],
+    declarations: [
+        ResponsiveHelperComponent,
+        UserTableComponent,
+        ClickOutsideDirective,
+    ],
+    imports: [
+        CommonModule,
+
+        // angular material
+        MatIconModule
+    ],
+    exports: [
+        ResponsiveHelperComponent,
+        UserTableComponent,
+        ClickOutsideDirective,
+        MatIconModule
+    ],
 })
-export class SharedModule {}
+export class SharedModule { }
