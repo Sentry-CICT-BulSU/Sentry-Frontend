3665d157183cf9aec04f1ba41a6d9b6f74b9e7ce
Allen Vincent <allenvincentbuning@gmail.com>
2023-03-31 15:36:25 +0800

    user-table in shared module

---
+++ src/app/shared/components/user-table/user-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+})
+export class UserTableComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
