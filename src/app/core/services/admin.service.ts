74f7c85d3ceecc1d6fb3f9888f3ba31fe6c796cc
rhonstratos <kizunaai20@gmail.com>
2023-03-31 14:51:20 +0800

    get all users

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -1,3 +1,4 @@
+import { IUser, IUserResponse } from './../models/user.model';
 import { environment as env } from './../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -18,29 +19,43 @@ export class AdminService {
 
     constructor(private http: HttpClient) { }
 
+    getUsers() {
+        return this.http.get<IUserResponse>(
+            this.adminApiRoute + '/users',
+            { headers: this.options.headers }
+        );
+    }
+
+    getUser(user: IUser) {
+        return this.http.get<IUserResponse>(
+            this.adminApiRoute + '/users' + user.id,
+            { headers: this.options.headers }
+        );
+    }
+
     createUser(newUser: IUserConfig) {
-        return this.http.post(
+        return this.http.post<IUserResponse>(
             this.adminApiRoute + '/users',
             newUser, { headers: this.options.headers }
         );
     }
 
     editUser(updatedUser: IUserConfig) {
-        return this.http.patch(
+        return this.http.patch<IUserResponse>(
             this.adminApiRoute + '/users/' + updatedUser.id,
             updatedUser, { headers: this.options.headers }
         );
     }
 
     softDeleteUser(deleteUser: IUserConfig) {
-        return this.http.delete(
+        return this.http.delete<IUserResponse>(
             this.adminApiRoute + '/users/' + deleteUser.id,
             { headers: this.options.headers }
         );
     }
 
     restoreUser(restoreUser: IUserConfig) {
-        return this.http.post(
+        return this.http.post<IUserResponse>(
             this.adminApiRoute + '/users/' + restoreUser.id + '/restore',
             null, { headers: this.options.headers }
         );
--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,18 +1,26 @@
+import { AdminService } from './../../core/services/admin.service';
+import { IUser, IUserResponse } from './../../core/models/user.model';
 // Importing necessary modules from @angular
 import { Component, OnInit } from '@angular/core';
 
 // Defining a new component with the selector 'app-dashboard' and the template URL 'dashboard.component.html'
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
+    selector: 'app-user',
+    templateUrl: './user.component.html',
 })
 
 // Exporting the DashboardComponent class and implementing the OnInit interface
 export class UserComponent implements OnInit {
+    user_pagination: any;
 
-  // Defining a constructor for the DashboardComponent class
-  constructor() {}
+    // Defining a constructor for the DashboardComponent class
+    constructor(private adminService: AdminService) { }
 
-  // Implementing the ngOnInit lifecycle hook
-  ngOnInit(): void {}
+    // Implementing the ngOnInit lifecycle hook
+    ngOnInit(): void {
+        this.adminService.getUsers().subscribe((response: IUserResponse) => {
+            this.user_pagination = response.paginate;
+            console.log(this.user_pagination);
+        });
+    }
 }
