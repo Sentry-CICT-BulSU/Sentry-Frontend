a25da565791c378899fe0be5ea79570ff3a4b0ff
rhonstratos <kizunaai20@gmail.com>
2023-03-31 15:04:51 +0800

    user model update

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -11,7 +11,7 @@ export interface IUser {
     email_verified_at?: string;
     created_at: string;
     updated_at: string;
-    deleted_at: string;
+    deleted_at?: string;
 }
 
 export interface IUserTypes {
