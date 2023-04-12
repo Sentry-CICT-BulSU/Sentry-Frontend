ea2c49338b96c457a0bbd3df6a2b3d547be9b657
Allen Vincent <allenvincentbuning@gmail.com>
2023-04-12 15:35:57 +0800

    room-keys to shared module

---
+++ src/app/shared/components/room-keys/room-keys.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-keys',
+  templateUrl: './room-keys.component.html',
+})
+export class RoomKeysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
