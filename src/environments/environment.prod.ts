41c8dbdb394c592e4f363b4491e6348b16f1cc37
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-12 22:29:49 +0800

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
-  production: true,
-  apiRootRoute: 'http://app.la-passport.test:4200'
+    production: true,
+    apiRootRoute: 'http://sentry-backend.up.railway.app',
+    clientRoot: 'http://sentry-frontend.up.railway.app:4200',
+    clientId: '2',
+    clientSecret: 'M14ZncSPP7dTzXjXpYzuaQImjBUvJOd3QfYsg7TJ',
+
+    oauthClientId: '1',
+    oauthLoginUrl: 'http://sentry-backend.up.railway.app/oauth/authorize',
+    oauthTokenUrl: 'http://sentry-backend.up.railway.app/oauth/token',
+    oauthCallbackUrl: 'http://sentry-frontend.up.railway.app:4200/auth/callback',
 };
