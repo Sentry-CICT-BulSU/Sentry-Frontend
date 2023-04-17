bd704c4364b95ef87b7c269848746d68ca64f418
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-17 15:39:08 +0800

    get all semesters

--- src/app/core/services/semester.service.ts
+++ src/app/core/services/semester.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { HttpClient } from '@angular/common/http';
-import { ISemester } from '../models/semester.model';
+import { ISemester, ISemesterCollection } from '../models/semester.model';
 import { IUser } from '../models';
 import { environment } from 'src/environments/environment';
 import { PropertiesService } from './properties.service';
@@ -18,7 +18,7 @@ export class SemesterService extends PropertiesService {
         );
     }
 
-    get loadSemesters() {
+    loadSemesters$() {
         let url;
         if (this.user?.type === 'Admin') {
             url = environment.apiRootRoute + '/api/admin/semesters';
@@ -30,10 +30,12 @@ export class SemesterService extends PropertiesService {
         } else {
             throw new Error('User is not an admin');
         }
-        return this.http.get<ISemester>(url, { headers: this.options.headers });
+        return this.http.get<ISemesterCollection>(url, {
+            headers: this.options.headers,
+        });
     }
 
-    loadSemester(id: ISemester['id']) {
+    loadSemester$(id: ISemester['id']) {
         let url;
         if (this.user?.type === 'Admin') {
             url = environment.apiRootRoute + '/api/admin/semesters/' + id;
@@ -45,6 +47,6 @@ export class SemesterService extends PropertiesService {
         } else {
             throw new Error('User is anonymous');
         }
-        return this.http.get<ISemester>(url, { headers: this.options.headers });
+        return this.http.get<ISemesterCollection>(url, { headers: this.options.headers });
     }
 }
--- src/app/modules/semester/pages/semester-list/semester-list.component.ts
+++ src/app/modules/semester/pages/semester-list/semester-list.component.ts
@@ -1,35 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ISemesterCollection } from 'src/app/core/models';
+import { SemesterService } from 'src/app/core/services/semester.service';
 
 @Component({
     selector: 'app-semester-list',
     templateUrl: './semester-list.component.html',
 })
 export class SemesterListComponent implements OnInit {
+    semesterCollection?: ISemesterCollection;
+    constructor(private semesterService: SemesterService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-    const tabLinks = document.querySelectorAll('.tab-link') as NodeListOf<HTMLAnchorElement>;
-    const tabContents = document.querySelectorAll('.tab-content') as NodeListOf<HTMLElement>;
-
-    tabLinks[0].classList.add('active');
-    tabContents[0].classList.add('active');
-
-    tabLinks.forEach((link: HTMLAnchorElement) => {
-      link.addEventListener('click', (event: Event) => {
-        event.preventDefault();
-        const selectedTab = link.hash;
-        tabLinks.forEach((link) => {
-          link.classList.remove('active');
+    ngOnInit(): void {
+        this.semesterService.loadSemesters$().subscribe((semester) => {
+            this.semesterCollection = semester as ISemesterCollection;
+            console.log(this.semesterCollection);
         });
-        tabContents.forEach((content) => {
-          content.classList.remove('active');
-        });
-        link.classList.add('active');
-        (document.querySelector(selectedTab) as HTMLElement).classList.add('active');
-      });
-    });
-  }
+        this.initComponent();
+    }
 
+    initComponent() {
+        const tabLinks = document.querySelectorAll(
+            '.tab-link'
+        ) as NodeListOf<HTMLAnchorElement>;
+        const tabContents = document.querySelectorAll(
+            '.tab-content'
+        ) as NodeListOf<HTMLElement>;
+
+        tabLinks[0].classList.add('active');
+        tabContents[0].classList.add('active');
+
+        tabLinks.forEach((link: HTMLAnchorElement) => {
+            link.addEventListener('click', (event: Event) => {
+                event.preventDefault();
+                const selectedTab = link.hash;
+                tabLinks.forEach((link) => {
+                    link.classList.remove('active');
+                });
+                tabContents.forEach((content) => {
+                    content.classList.remove('active');
+                });
+                link.classList.add('active');
+                (
+                    document.querySelector(selectedTab) as HTMLElement
+                ).classList.add('active');
+            });
+        });
+    }
 }
-
