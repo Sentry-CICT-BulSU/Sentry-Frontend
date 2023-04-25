1e76a4b7dde9d6124695d8c07d903d6b22fef231
Allen Vincent <allenvincentbuning@gmail.com>
2023-04-26 03:58:26 +0800

    edit room page

---
+++ src/app/modules/room/pages/edit-room/edit-room.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-edit-room',
+    templateUrl: './edit-room.component.html',
+})
+export class EditRoomComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+}
+}
--- src/app/modules/subject/pages/edit-subject/edit-subject.component.ts
+++ src/app/modules/subject/pages/edit-subject/edit-subject.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditSubjectComponent implements OnInit {
 
-    ngOnInit(): void {
+  constructor() { }
 
-    }
+    ngOnInit() {
+  }
 
 }
