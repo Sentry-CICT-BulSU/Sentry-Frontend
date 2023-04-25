bfa3bd12b591d6d0c2717106bfbc98cf76d1504f
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-26 02:50:30 +0800

    subject module

--- src/app/modules/subject/pages/subject-list/subject-list.component.ts
+++ src/app/modules/subject/pages/subject-list/subject-list.component.ts
@@ -1,49 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-import { ISubjectCollection } from 'src/app/core/models';
+import { Router } from '@angular/router';
+import { ISubject, ISubjectCollection } from 'src/app/core/models';
 import { SubjectService } from 'src/app/core/services/subject.service';
 
 @Component({
-    selector: 'app-subject-list',
-    templateUrl: './subject-list.component.html',
+  selector: 'app-subject-list',
+  templateUrl: './subject-list.component.html',
 })
 export class SubjectListComponent implements OnInit {
-    subjectCollection?: ISubjectCollection;
-    constructor(private subjectService: SubjectService) {}
+  subjectCollection?: ISubjectCollection;
+  activeSubjectCollection?: ISubjectCollection;
+  inactiveSubjectCollection?: ISubjectCollection;
 
-    ngOnInit(): void {
-        this.initComponent();
-        this.subjectService.loadSubjects$().subscribe((subject) => {
-            this.subjectCollection = subject;
-            console.log(subject);
+  subjects?: ISubject[];
+  activeSubjects?: ISubject[];
+  inactiveSubjects?: ISubject[];
+  constructor(private subjectService: SubjectService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.initComponent();
+    this.loadSubjects();
+    this.loadActiveSubjects();
+    this.loadInactiveSubjects();
+  }
+
+  onEdit(id: number) {
+    console.log('edit subject');
+  }
+  onDelete(id: number) {
+    this.subjectService.deleteSubject$(id).subscribe({
+      next: (resp) => {
+        console.log(resp);
+        this.router.navigate(['/subject']);
+      },
+    });
+  }
+
+  loadSubjects() {
+    this.subjectService.loadSubjects$().subscribe((subject) => {
+      this.subjectCollection = subject;
+      this.subjects = subject.data as ISubject[];
+      console.log(subject);
+    });
+  }
+  loadActiveSubjects() {
+    const query = { q: 'active' };
+    this.subjectService.loadSubjects$(query).subscribe((subject) => {
+      this.subjectCollection = subject;
+      this.activeSubjects = subject.data as ISubject[];
+      console.log(subject);
+    });
+  }
+  loadInactiveSubjects() {
+    const query = { q: 'inactive' };
+    this.subjectService.loadSubjects$(query).subscribe((subject) => {
+      this.subjectCollection = subject;
+      this.inactiveSubjects = subject.data as ISubject[];
+      console.log(subject);
+    });
+  }
+
+  initComponent() {
+    const tabLinks = document.querySelectorAll(
+      '.tab-link'
+    ) as NodeListOf<HTMLAnchorElement>;
+    const tabContents = document.querySelectorAll(
+      '.tab-content'
+    ) as NodeListOf<HTMLElement>;
+
+    tabLinks[0].classList.add('active');
+    tabContents[0].classList.add('active');
+
+    tabLinks.forEach((link: HTMLAnchorElement) => {
+      link.addEventListener('click', (event: Event) => {
+        event.preventDefault();
+        const selectedTab = link.hash;
+        tabLinks.forEach((link) => {
+          link.classList.remove('active');
         });
-    }
-
-    initComponent() {
-        const tabLinks = document.querySelectorAll(
-            '.tab-link'
-        ) as NodeListOf<HTMLAnchorElement>;
-        const tabContents = document.querySelectorAll(
-            '.tab-content'
-        ) as NodeListOf<HTMLElement>;
-
-        tabLinks[0].classList.add('active');
-        tabContents[0].classList.add('active');
-
-        tabLinks.forEach((link: HTMLAnchorElement) => {
-            link.addEventListener('click', (event: Event) => {
-                event.preventDefault();
-                const selectedTab = link.hash;
-                tabLinks.forEach((link) => {
-                    link.classList.remove('active');
-                });
-                tabContents.forEach((content) => {
-                    content.classList.remove('active');
-                });
-                link.classList.add('active');
-                (
-                    document.querySelector(selectedTab) as HTMLElement
-                ).classList.add('active');
-            });
+        tabContents.forEach((content) => {
+          content.classList.remove('active');
         });
-    }
+        link.classList.add('active');
+        (document.querySelector(selectedTab) as HTMLElement).classList.add(
+          'active'
+        );
+      });
+    });
+  }
 }
