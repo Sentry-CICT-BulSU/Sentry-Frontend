1c6efab22a51d717794578df7c6391df77c40f1f
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-25 23:49:36 +0800

    hide logs in dashboard if empty

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -5,34 +5,29 @@ import { RoomKeyLogsService } from 'src/app/core/services/roomkey-logs.service';
 
 // Defining a new component with the selector 'app-dashboard' and the template URL 'dashboard.component.html'
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
 })
 
 // Exporting the DashboardComponent class and implementing the OnInit interface
 export class DashboardComponent implements OnInit {
-    roomKeyLogs?: IRoomKeyLog[];
-    roomKeyLogsCollection?: IRoomKeyLogCollection;
-    // Defining a constructor for the DashboardComponent class
-    constructor(private roomKeyLogsService: RoomKeyLogsService) {}
+  roomKeyLogs?: IRoomKeyLog[];
+  roomKeyLogsCollection?: IRoomKeyLogCollection;
+  logIsEmpty = false;
+  // Defining a constructor for the DashboardComponent class
+  constructor(private roomKeyLogsService: RoomKeyLogsService) {}
 
-    // Implementing the ngOnInit lifecycle hook
-    ngOnInit(): void {
-        this.roomKeyLogsService
-            .getRoomKeyLogs$()
-            .subscribe((logs: IRoomKeyLogCollection) => {
-                this.roomKeyLogsCollection = logs;
-                if (
-                    this.roomKeyLogsCollection.data &&
-                    Array.isArray(this.roomKeyLogsCollection.data)
-                ) {
-                    this.roomKeyLogs = this.roomKeyLogsCollection.data;
-                }
-                console.debug('Room Key Logs', this.roomKeyLogs);
-                console.debug(
-                    'Room Key Logs Collection',
-                    this.roomKeyLogsCollection
-                );
-            });
-    }
+  // Implementing the ngOnInit lifecycle hook
+  ngOnInit(): void {
+    this.roomKeyLogsService.getRoomKeyLogs$().subscribe({
+      next: (logs: IRoomKeyLogCollection) => {
+        this.roomKeyLogsCollection = logs;
+        this.roomKeyLogs = this.roomKeyLogsCollection.data as IRoomKeyLog[];
+        this.logIsEmpty = this.roomKeyLogs.length === 0;
+        console.debug('Room Key Logs', this.roomKeyLogs);
+        console.debug('Room Key Logs Collection', this.roomKeyLogsCollection);
+        console.debug('Room Key Logs is empty', this.logIsEmpty);
+      },
+    });
+  }
 }
