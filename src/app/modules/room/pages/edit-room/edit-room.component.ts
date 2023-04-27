6076e21f5096d075d6d1c427cf1828cbb7ce5f17
Allen Vincent <allenvincentbuning@gmail.com>
2023-04-28 01:07:31 +0800

    swal updates

--- src/app/modules/room/pages/edit-room/edit-room.component.ts
+++ src/app/modules/room/pages/edit-room/edit-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-edit-room',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditRoomComponent implements OnInit {
   constructor() { }
+
+  onSubmit() {
+
+    //add code here
+
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Room edited successfully!',
+          });
+  }
 
   ngOnInit() {
 }
