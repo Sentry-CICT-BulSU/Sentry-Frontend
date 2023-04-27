e92f9aebb8ab88921819ebee349a332ff646f45b
Allen Vincent <allenvincentbuning@gmail.com>
2023-04-28 04:03:55 +0800

    swal changes

--- src/app/modules/subject/pages/edit-subject/edit-subject.component.ts
+++ src/app/modules/subject/pages/edit-subject/edit-subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-edit-subject',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class EditSubjectComponent implements OnInit {
 
   constructor() { }
+
+  onSubmit() {
+
+    //add code here
+
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Subject edited successfully!',
+          });
+  }
 
     ngOnInit() {
   }
