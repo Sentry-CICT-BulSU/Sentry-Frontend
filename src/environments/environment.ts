409f5cb13f0b4560168408043ea70abc858653e0
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-01 19:09:53 +0800

    multi dev routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,13 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const app_url = 'http://localhost:4200';
-const api_url = 'https://sentry-backend.up.railway.app';
+
+// const app_url = 'http://localhost:4200';
+// const api_url = 'https://sentry-backend.up.railway.app';
+
+const app_url = 'http://app.cict-sentry.test:4200';
+const api_url = 'http://cict-sentry.test';
+
 export const environment = {
   production: false,
   apiRootRoute: api_url,
