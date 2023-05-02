cce47ed7ce692dac0c76621bfbc6fd265021e904
Cramzzzberry <janroebantuan@gmail.com>
2023-05-02 22:07:11 +0800

    fixed some ui elements and moved some buttons to other pages

--- src/app/modules/keys/pages/keys-overview/keys-overview.component.ts
+++ src/app/modules/keys/pages/keys-overview/keys-overview.component.ts
@@ -6,9 +6,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeysOverviewComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    this.initComponent();
+  }
+
+  initComponent() {
+    const tabLinks = document.querySelectorAll(
+        '.tab-link'
+    ) as NodeListOf<HTMLAnchorElement>;
+    const tabContents = document.querySelectorAll(
+        '.tab-content'
+    ) as NodeListOf<HTMLElement>;
+
+    tabLinks[0].classList.add('active');
+    tabContents[0].classList.add('active');
+
+    tabLinks.forEach((link: HTMLAnchorElement) => {
+        link.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+            const selectedTab = link.hash;
+            tabLinks.forEach((link) => {
+                link.classList.remove('active');
+            });
+            tabContents.forEach((content) => {
+                content.classList.remove('active');
+            });
+            link.classList.add('active');
+            (
+                document.querySelector(selectedTab) as HTMLElement
+            ).classList.add('active');
+        });
+    });
   }
 
 }
