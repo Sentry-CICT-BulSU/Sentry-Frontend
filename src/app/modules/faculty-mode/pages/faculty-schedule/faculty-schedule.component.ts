82d38ef256fafcc77c4fd25238d1f9e01f7029ba
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-03 15:20:43 +0800

    load faculty schedules

--- src/app/modules/faculty-mode/pages/faculty-schedule/faculty-schedule.component.ts
+++ src/app/modules/faculty-mode/pages/faculty-schedule/faculty-schedule.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, of, switchMap } from 'rxjs';
+import { ISchedule, IUser } from 'src/app/core/models';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ScheduleService } from 'src/app/core/services/schedule.service';
 
 interface ScheduleData {
   day: string;
@@ -13,19 +17,81 @@ interface ScheduleData {
   templateUrl: './faculty-schedule.component.html',
 })
 export class FacultyScheduleComponent implements OnInit {
-  daysOfWeek: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  timeslots: string[] = ['7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm'];
+  daysOfWeek: string[] = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  timeslots: string[] = [
+    '7:00 am',
+    '8:00 am',
+    '9:00 am',
+    '10:00 am',
+    '11:00 am',
+    '12:00 pm',
+    '1:00 pm',
+    '2:00 pm',
+    '3:00 pm',
+    '4:00 pm',
+    '5:00 pm',
+    '6:00 pm',
+  ];
   scheduleData: ScheduleData[] = [
-    { day: 'Wednesday', timeStart: '9:00 am', timeEnd: '11:00 am', subject: 'Capstone 2', room: 'IT1' },
-    { day: 'Monday', timeStart: '7:00 am', timeEnd: '10:00 am', subject: 'Networking 2', room: 'IT2' },
+    {
+      day: 'Wednesday',
+      timeStart: '9:00 am',
+      timeEnd: '11:00 am',
+      subject: 'Capstone 2',
+      room: 'IT1',
+    },
+    {
+      day: 'Monday',
+      timeStart: '7:00 am',
+      timeEnd: '10:00 am',
+      subject: 'Networking 2',
+      room: 'IT2',
+    },
   ];
 
-  constructor() { }
+  schedules?: ISchedule[];
+  user?: IUser;
 
-  ngOnInit(): void { }
+  constructor(
+    private schedulesService: ScheduleService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadSchedules();
+  }
+
+  loadSchedules() {
+    this.authService.current_user_subject$
+      ?.pipe(
+        filter((user) => !!user),
+        switchMap((user) =>
+          user
+            ? this.schedulesService.loadSchedules$({ fid: user.id })
+            : of(undefined)
+        )
+      )
+      .subscribe({
+        next: (schedules) => {
+          if (schedules) {
+            this.schedules = schedules.data as ISchedule[];
+            console.log(schedules);
+          }
+        },
+        error: (err) => console.debug(err),
+      });
+  }
 
   isSlotFilled(day: string, time: string): ScheduleData | undefined {
-    return this.scheduleData.find(schedule => {
+    return this.scheduleData.find((schedule) => {
       if (schedule.day !== day) {
         return false;
       }
@@ -36,16 +102,13 @@ export class FacultyScheduleComponent implements OnInit {
     });
   }
 
-
   getSubject(day: string, time: string): string | undefined {
     const slot = this.isSlotFilled(day, time);
     return slot && slot.timeStart === time ? slot.subject : undefined;
-}
+  }
 
-getRoom(day: string, time: string): string | undefined {
+  getRoom(day: string, time: string): string | undefined {
     const slot = this.isSlotFilled(day, time);
     return slot && slot.timeStart === time ? slot.room : undefined;
-}
-
-
+  }
 }
