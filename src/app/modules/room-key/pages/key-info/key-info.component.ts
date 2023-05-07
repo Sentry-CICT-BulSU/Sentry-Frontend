04ac3ddf0a116d76ea8b0abbba0cee4fa6e14d1b
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-08 03:19:12 +0800

    fix room key info

--- src/app/modules/room-key/pages/key-info/key-info.component.ts
+++ src/app/modules/room-key/pages/key-info/key-info.component.ts
@@ -43,17 +43,10 @@ export class KeyInfoComponent implements OnInit {
   }
 
   loadSubs() {
-    if (!this.roomKeyId) return;
-
-    console.log(this.roomKeyId);
-    return this.roomKeyService
-      .getRoomKey$(this.roomKeyId)
-      .subscribe((roomKey) => {
-        if (
-          (roomKey.data as IRoomKey) &&
-          (roomKey.data as IRoomKey).logs &&
-          ((roomKey.data as IRoomKey).schedules as ISchedule[]).length > 0
-        ) {
+    if (this.roomKeyId) {
+      console.log(this.roomKeyId);
+      this.roomKeyService.getRoomKey$(this.roomKeyId).subscribe((roomKey) => {
+        if (roomKey) {
           this.roomKey = roomKey.data as IRoomKey;
           this.logs = this.roomKey.logs as IRoomKeyLog[];
           this.schedules = this.roomKey.schedules as ISchedule[];
@@ -64,6 +57,7 @@ export class KeyInfoComponent implements OnInit {
           this.loadFormControls();
         }
       });
+    }
   }
   initForm() {
     this.roomKeyForm = this.fb.group({
