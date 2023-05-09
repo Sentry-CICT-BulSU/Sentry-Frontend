9d4fb921d6dd88f338676c6e0b94025f47eb9742
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-09 15:13:29 +0800

    bind room key stats to admin dashboard

--- src/app/modules/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
@@ -2,7 +2,11 @@ import { ScheduleService } from './../../../../core/services/schedule.service';
 import { forkJoin } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { RoomKeyLogsService } from 'src/app/core/services/roomkey-logs.service';
-import { IRoomKeyLog, IRoomKeyLogCollection } from 'src/app/core/models';
+import {
+  IAvailableKeys,
+  IRoomKeyLog,
+  IRoomKeyLogCollection,
+} from 'src/app/core/models';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -16,6 +20,13 @@ export class AdminDashboardComponent implements OnInit {
     absents: 0,
     not_visited: 0,
   };
+  availableKeys: IAvailableKeys = {
+    available: 0,
+    available_percentage: 0,
+    unavailable: 0,
+    unavailable_percentage: 0,
+    total_keys: 0,
+  };
   // Defining a constructor for the DashboardComponent class
   constructor(
     private roomKeyLogsService: RoomKeyLogsService,
@@ -27,7 +38,8 @@ export class AdminDashboardComponent implements OnInit {
     forkJoin([
       this.roomKeyLogsService.getRoomKeyLogs$(),
       this.schedulesService.loadSchedules$({ 'admin-dash': true }),
-    ]).subscribe(([logs, schedules]) => {
+      this.roomKeyLogsService.getAvailableRoomKeys$(),
+    ]).subscribe(([logs, schedules, available]) => {
       console.log(logs, schedules);
       this.attendanceToday = schedules;
       this.roomKeyLogsCollection = logs;
@@ -36,6 +48,9 @@ export class AdminDashboardComponent implements OnInit {
         Array.isArray(this.roomKeyLogsCollection.data)
       ) {
         this.roomKeyLogs = this.roomKeyLogsCollection.data;
+      }
+      if (available) {
+        this.availableKeys = available;
       }
     });
   }
