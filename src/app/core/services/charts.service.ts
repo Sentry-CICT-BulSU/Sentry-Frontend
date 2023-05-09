ce53046a9513025709d5a8490421ef5d6899ab0b
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-10 03:00:31 +0800

    admin charts

---
+++ src/app/core/services/charts.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IUser } from '../models';
+import { AuthService } from './auth.service';
+import { environment as env } from 'src/environments/environment';
+import { PropertiesService } from './properties.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChartsService extends PropertiesService {
+  user?: IUser;
+  constructor(private http: HttpClient, private auth: AuthService) {
+    super();
+    auth.current_user_subject$?.subscribe((user) => (this.user = user));
+  }
+
+  get url() {
+    return env.apiRootRoute + '/api/charts';
+  }
+
+  loadCharts$() {
+    return this.http.get<any>(this.url, { headers: this.options.headers });
+  }
+}
--- src/app/modules/dashboard/chart-card/chart-card.component.ts
+++ src/app/modules/dashboard/chart-card/chart-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ChartsService } from 'src/app/core/services/charts.service';
 import { ThemeService } from 'src/app/core/services/theme.service';
 import { ChartOptions } from 'src/app/shared/models/chart-options';
 
@@ -10,37 +11,39 @@ import { ChartOptions } from 'src/app/shared/models/chart-options';
 export class ChartCardComponent implements OnInit, OnDestroy {
   public chartOptions: Partial<ChartOptions>;
   private subscription: Subscription = new Subscription();
-
-  constructor(private themeService: ThemeService) {
-    const data = [0, 0, 0, 5, 4, 0, 2];
-    const data2 = [0, 0, 0, 3, 4, 8, 6];
-    const categories = [
-        "7 days ago",
-        "6 days ago",
-        "5 days ago",
-        "4 days ago",
-        "3 days ago",
-        "2 days ago",
-        "Yesterday",
-    ];
-
+  data = [0, 0, 0, 5, 4, 0, 2];
+  data2 = [0, 0, 0, 3, 4, 8, 6];
+  categories = [
+    '6 days ago',
+    '5 days ago',
+    '4 days ago',
+    '3 days ago',
+    '2 days ago',
+    'Yesterday',
+    'Today',
+  ];
+  constructor(
+    private themeService: ThemeService,
+    private chartService: ChartsService
+  ) {
     this.chartOptions = {
       series: [
         {
           name: 'Present',
-          data: data,
-          color: '#7239ea'
+          data: this.data,
+          color: '#7239ea',
         },
         {
           name: 'Absent',
-          data: data2,
-          color: '#D6BBFB'
-        }
+          data: this.data2,
+          color: '#D6BBFB',
+        },
       ],
       chart: {
         fontFamily: 'inherit',
         type: 'area',
         height: 150,
+        stacked: true,
         toolbar: {
           show: false,
         },
@@ -66,7 +69,7 @@ export class ChartCardComponent implements OnInit, OnDestroy {
         width: 3,
       },
       xaxis: {
-        categories: categories,
+        categories: this.categories,
         labels: {
           show: false,
         },
@@ -82,21 +85,43 @@ export class ChartCardComponent implements OnInit, OnDestroy {
           enabled: true,
         },
       },
-    //   tooltip: {
-    //     theme: 'light',
-    //     y: {
-    //       formatter: function (val) {
-    //         return val + '$';
-    //       },
-    //     },
-    //   },
+      //   tooltip: {
+      //     theme: 'light',
+      //     y: {
+      //       formatter: function (val) {
+      //         return val + '$';
+      //       },
+      //     },
+      //   },
       colors: ['#7239ea', '#D6BBFB'], // set colors for each series
     };
   }
 
   ngOnInit(): void {
     /** Chand chart theme */
-    let sub = this.themeService.themeChanged.subscribe((theme) => {
+    this.initComponent();
+    this.chartService.loadCharts$().subscribe({
+      next: (resp: any) => {
+        this.chartOptions.series = [
+          {
+            name: 'Present',
+            data: resp.presentees,
+            color: '#7239ea',
+          },
+          {
+            name: 'Absent',
+            data: resp.absentees,
+            color: '#D6BBFB',
+          },
+        ];
+        console.log('apexcharts: ', resp);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  initComponent() {
+    const sub = this.themeService.themeChanged.subscribe((theme) => {
       this.chartOptions.tooltip = {
         theme: theme,
       };
