23707ffbb1e087025eccd9d66bc727bc54b686ff
Allen Vincent <allenvincentbuning@gmail.com>
2023-05-11 01:49:43 +0800

    logout button for mobile

--- src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobilecomponent.ts
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobilecomponent.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MenuItem } from 'src/app/core/models/menu.model';
 import { MenuService } from '../../../services/menu.service';
 import { SystemService } from 'src/app/core/services/system.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar-mobile',
@@ -13,6 +14,8 @@ export class NavbarMobileComponent implements OnInit {
   public showMobileMenu$: Observable<boolean> = new Observable<boolean>();
   sysIcon?: string;
   sysName?: string;
+
+
   constructor(private menuService: MenuService, private system: SystemService) {
     this.showMobileMenu$ = this.menuService.showMobileMenu$;
   }
@@ -24,5 +27,22 @@ export class NavbarMobileComponent implements OnInit {
 
   public toggleMobileMenu(): void {
     this.menuService.showMobileMenu = false;
+  }
+
+  onSignOut() {
+    // Show confirmation dialog using Swiperalert2
+    Swal.fire({
+      title: 'Confirmation',
+      text: 'Are you sure you want to sign out?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, sign out',
+      cancelButtonText: 'Cancel',
+    }).then((result: { isConfirmed: any }) => {
+      // If user confirms, emit signOut event
+      if (result.isConfirmed) {
+        console.log('Sign out confirmed');
+      }
+    });
   }
 }
